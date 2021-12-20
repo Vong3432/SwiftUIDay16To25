d322eab7c1485f26ace7dfae03d0c02df3a66bbe
Vong3432 <vongnyuksoon2000@gmail.com>
2021-12-20 17:23:39 +0800

    first commit

---
+++ SwiftUIDay16To25/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDay16To25
+//
+//  Created by Vong Nyuksoon on 20/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDay16To25/SwiftUIDay16To25App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDay16To25App.swift
+//  SwiftUIDay16To25
+//
+//  Created by Vong Nyuksoon on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDay16To25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
